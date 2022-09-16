c255288f879f6e17636104d5ba306f31a45a7a96
Olesya <lesusha1@yandex.ru>
2022-09-16 15:07:26 +0300

    add Task_25

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -12,7 +12,7 @@ Console.WriteLine(Exponent(numberA, numberB));
 int Exponent(int numA, int numB)
 {
     int result = 1;
-    for (int i = 1; i <= numB; i++)
+    for (int i = 0; i < numB; i++)
     {
         result = result * numA;
     }
